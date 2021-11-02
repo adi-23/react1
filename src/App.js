d8a826020e6978ceb1125da71c8c0c3fe46ad251
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2021-11-02 19:05:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  //const [todos,setTodos] =useState([])
+  // const todoNameRef = useRef()
+  // function handleAddTodo(e){
+  //   const name = todoNameRef.current.value
+  //   if (name === ''){
+  //     setTodos(prevTodos => {
+  //       return [...prevTodos,{id: 1,name: name, complete: false}]
+  //     })
+  //   }
+  //   todoNameRef.current.value = null
 
+
+
+  return (
+    <>
+      
+      
+      
+      <button>Clear</button>
+      <div>0 left to do</div>
+    </>
+  );
+
+}
 export default App;
